6912941e856e537a61b24851b8d514dcff769ca8
yasmindeoliveira <yaz.oliveira01@gmail.com>
2023-01-24 16:43:43 +0100

    creato controller API

---
+++ LaMiaPizzeriaEfPost/Controllers/PizzaAPIController.cs
@@ -0,0 +1,24 @@
+﻿using LaMiaPizzeriaEfPost.Database;
+using LaMiaPizzeriaEfPost.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LaMiaPizzeriaEfPost.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            using(PizzaContext db = new PizzaContext())
+            {
+                List<Pizza> pizzaApi = db.Pizzas.Include(pizzaApi => pizzaApi.Ingredients).ToList();
+
+                return Ok(pizzaApi);
+            }
+        }
+    }
+}
